5bde91f1396cd895998ad9dc5959510b04e64fcc
Abbas Zaidi <abbasrazazaidi731@gmail.com>
2026-03-12 00:58:33 +0530

    api gateway implementation

--- analytics-service/src/main/java/org/example/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/org/example/kafka/KafkaConsumer.java
@@ -1,19 +1,23 @@
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import patient.events.PatientEvent;
-
+import org.example.kafka.PatientEvent;
 @Service
 public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @KafkaListener(topics = "patient", groupId = "analytics-service")
     public void consumeEvent(byte[] event) {
         try {
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
             // ... perform any business related to analytics here
+
 
             log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
                     patientEvent.getPatientId(),
